ec716443fece705e553c2a24eddea73ee3d835a2
Hareem Adderley <haddderley@kingpinapps.com>
2025-03-13 06:28:43 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-cardano-chain",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "swift-cardano-chain",
+            targets: ["swift-cardano-chain"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "swift-cardano-chain"),
+        .testTarget(
+            name: "swift-cardano-chainTests",
+            dependencies: ["swift-cardano-chain"]
+        ),
+    ]
+)
