045710ffaf5161d06dfd626c441dce59fa609984
Hareem Adderley <haddderley@kingpinapps.com>
2025-09-06 15:50:41 -0500

    fix: update core and improve tests

--- Tests/SwiftCardanoChainTests/BackendsTests/BlockfrostTests.swift
+++ Tests/SwiftCardanoChainTests/BackendsTests/BlockfrostTests.swift
@@ -12,20 +12,14 @@ struct BlockfrostChainContextTests {
             environmentVariable: "BLOCKFROST_API_KEY"
         )
         
-        let genesisParam = try await chainContext.genesisParameters()
-        let protocolParam = try await chainContext.protocolParameters()
-        let lastBlockSlot = try await chainContext.lastBlockSlot()
-        let epoch = try await chainContext.epoch()
+        _ = try await chainContext.genesisParameters()
+        _ = try await chainContext.protocolParameters()
+        _ = try await chainContext.lastBlockSlot()
+        _ = try await chainContext.epoch()
         let network = chainContext.network
         
-        let utxos = try await chainContext.utxos(address: Address(from: .string("addr_test1qp4kux2v7xcg9urqssdffff5p0axz9e3hcc43zz7pcuyle0e20hkwsu2ndpd9dh9anm4jn76ljdz0evj22stzrw9egxqmza5y3")))
+        _ = try await chainContext.utxos(address: Address(from: .string("addr_test1qp4kux2v7xcg9urqssdffff5p0axz9e3hcc43zz7pcuyle0e20hkwsu2ndpd9dh9anm4jn76ljdz0evj22stzrw9egxqmza5y3")))
         
-        #expect(chainContext != nil)
-        #expect(genesisParam != nil)
-        #expect(protocolParam != nil)
-        #expect(lastBlockSlot != nil)
-        #expect(epoch != nil)
-        #expect(utxos != nil)
         #expect(network == .testnet)
     }
 }
