bf021181305bad775b9764ae65804ea6624cdd1e
Hareem Adderley <hadderley@kingpinapps.com>
2026-09-23 20:37:40 -0500

    chore: require swift-cardano-uplc 0.5.0

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .package(url: "https://github.com/Kingpin-Apps/swift-ogmios.git", from: "0.3.2"),
         .package(url: "https://github.com/Kingpin-Apps/swift-yaci-api.git", from: "0.1.0"),
         .package(url: "https://github.com/Kingpin-Apps/swift-cardano-network.git", from: "1.1.1"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-uplc.git", from: "0.4.0"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-uplc.git", from: "0.5.0"),
         // Direct dep (was transitive via SwiftCardanoUtils, now trait-gated out): OfflineTransfer
         // uses FilePath. Apple package, iOS-safe.
         .package(url: "https://github.com/apple/swift-system.git", from: "1.8.1"),
